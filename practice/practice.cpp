d89a9efa04d2f4093c589c9e8031f5df34cca14f
capeza <capeza007@gmail.com>
2024-04-22 17:42:14 +0300

    stl2

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iterator>
+#include <ostream>
 #include <vector>
 using namespace std;
 
@@ -7,11 +9,28 @@ int main()
     setlocale (LC_ALL, "Ru");
     
     vector<int> myVector = {1,5,7,34,3,2,43,5,6};
+    vector<int>::iterator i=myVector.begin();
 
-    vector<int>::iterator it;
-
-    it = myVector.begin();
     
-    cout<<*it<<endl;
+    myVector.erase(i, i+3);
+    
+
+
+    
+    
+    for (vector<int>::iterator i=myVector.begin(); i!=myVector.end(); ++i) {
+    cout<<*i<<endl;
+    }
+    
+    // cout<<"------------------------------------"<<endl;
+    
+    // for (vector<int>::reverse_iterator i=myVector.rbegin(); i!=myVector.rend(); ++i) {
+    // cout<<*i<<endl;
+    // }
+
+    // advance(i,4);
+    // cout<<*i;
+
+    // myVector.insert(i,999);
         return 0;
 }
---
+++ practice/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    auto br = "adasd";
+    cout<<"Hello world"<<endl;
+    return 0;
+}

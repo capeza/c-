1aff73fdfbe4ee9a7b29e0fb51f3912f9f8922bc
capeza <capeza007@gmail.com>
2024-04-26 11:32:08 +0300

    vector sturt

--- start on windows/main.cpp
+++ start on windows/main.cpp
@@ -1,53 +1,47 @@
-//1) Создать двумерный массив 5х5, заполнить его случайными числами от 
-//10 до 99 и вывести на экран.Поменять местами максимальный элемент 
-//каждой строки с первым элементом в соответствующей строке.Задачу 
-//решить с помощью указателей.
-
-
 #include <iostream>
-#include<ctime>
 using namespace std;
+template<typename type>
+class vector
+{
+private:
+	type* array;
+	int size{};
+	int number{};
+public:
+
+	vector()
+	{
+		array = new type[size];
+	}
+	vector(int size) : size(size)
+	{
+		array = new type[size];
+	}
+	vector(int size, int number) : size(size), number(number)
+	{
+		array = new type[size];
+		for (int i = 0; i < size; i++)
+		{
+			array[i] = number;
+		}
+	}
+	~vector()
+	{
+		delete []array;
+	}
+};
+
+
+
+
 
 int main()
 {
-	setlocale(LC_ALL, "Ru");
-	srand(time(NULL));
+    setlocale(LC_ALL, "ru");
+	
+	vector<int> numbers1(3);
+	vector<int> numbers2(3, 2);
 
-	int** arr = new int* [5];
-
-	for (int i = 0; i < 5; ++i)
-	{
-		arr[i] = new int[5];
-	}
-
-	for (int i = 0, max{}, num_max{}; i < 5; ++i)
-	{
-		max = 0;
-		num_max = 0;
-		for (int j = 0; j < 5; ++j)
-		{
-			arr[i][j] = rand() % 89 + 10;
-			if (arr[i][j] > max)
-			{
-				max = arr[i][j];
-				num_max = j;
-			}
-		}
-		swap(arr[i][0], arr[i][num_max]);
-	}
-
-	for (int i = 0; i < 5; ++i)
-	{
-		for (int j = 0; j < 5; ++j)
-		{
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
-	}
-
-	for (int i = 0; i < 5; ++i) {
-		delete[] arr[i];
-	}
-	delete[] arr;
-	return 0;
+    return 0;
 }
+
